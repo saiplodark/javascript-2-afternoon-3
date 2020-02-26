746902c92e634e02948dcf1fd9ccbe633fde5172
Li Jung Huang <lijunghuang@Lis-MacBook-Pro.local>
2020-02-26 16:19:00 -0700

    stuckat567

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first(arr, cb){
+  cb(arr[0])
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +51,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(arr, cb){
+  cb(arr[arr.length-1])
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,6 +72,9 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply(num1, num2, cb){
+  cb(num1 * num2)
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +94,10 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+
+function contains(arr, name, cb){
+ for(var i = 0; i < arr.length; i++){arr[i] === name ? cb(true) : cb(false)}
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +119,9 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq (arr, cb){
+
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -123,6 +139,9 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each(arr, cb){
+  
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
